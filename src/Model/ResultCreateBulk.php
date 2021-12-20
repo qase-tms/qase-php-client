4053ab3641b8de9ed1580fcc6888ec35aeded426
Sergey Skaridov <s.skaridov@gmail.com>
2021-12-20 18:30:06 +0300

    add attributes to prevent php8.1 notices

--- src/Model/ResultCreateBulk.php
+++ src/Model/ResultCreateBulk.php
@@ -241,6 +241,7 @@ class ResultCreateBulk implements ModelInterface, ArrayAccess, JsonSerializable
      *
      * @return boolean
      */
+    #[\ReturnTypeWillChange]
     public function offsetExists($offset)
     {
         return isset($this->container[$offset]);
@@ -253,6 +254,7 @@ class ResultCreateBulk implements ModelInterface, ArrayAccess, JsonSerializable
      *
      * @return mixed|null
      */
+    #[\ReturnTypeWillChange]
     public function offsetGet($offset)
     {
         return $this->container[$offset] ?? null;
@@ -266,6 +268,7 @@ class ResultCreateBulk implements ModelInterface, ArrayAccess, JsonSerializable
      *
      * @return void
      */
+    #[\ReturnTypeWillChange]
     public function offsetSet($offset, $value)
     {
         if (is_null($offset)) {
@@ -282,6 +285,7 @@ class ResultCreateBulk implements ModelInterface, ArrayAccess, JsonSerializable
      *
      * @return void
      */
+    #[\ReturnTypeWillChange]
     public function offsetUnset($offset)
     {
         unset($this->container[$offset]);
@@ -294,6 +298,7 @@ class ResultCreateBulk implements ModelInterface, ArrayAccess, JsonSerializable
      * @return mixed Returns data which can be serialized by json_encode(), which is a value
      * of any type other than a resource.
      */
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return ObjectSerializer::sanitizeForSerialization($this);
